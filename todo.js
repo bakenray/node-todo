689b41777d9299082e46f277fd9099e45f18856b
bakenray <lpqstrong@163.com>
2019-09-10 22:23:06 +0800

    简化代码，整体优化

--- todo.js
+++ todo.js
@@ -1,80 +1,82 @@
 var fs = require('fs')
+var path = require('path')
 
 const verb = process.argv[2]
 const content = process.argv[3]
 const content2 = process.argv[4]
-const dbPath = 'C:\\Users\\win\\desktop\\node-demo-1\\db' 
-// nodejs中/是用来转义的，使用/就需要/来转义，所以有两个//
 
-function save(list){ //保存到数据库
-    fs.writeFileSync(dbPath,JSON.stringify(list))
-}
+const dbPath = path.join(__dirname,'db')
 
-function fetch(){
-    const fileContent = fs.readFileSync(dbPath).toString()
-    list = JSON.parse(fileContent)
-    return list
-}
-function addTask(list,content){
-    list.push([content,false])
-}
-function removeTask(list,n){
-    list.splice(n-1,1)
-}
-function taskDone(list,n){
-    list[n-1][1] = true
-}
-function editTask(list,n,content2){
-    list[n-1][0] = content2
-}
+// /是用来转义，使用/，所以有两个//
 
-//确保文件一定存在
-try{//如果有报错，就执行catch
-    fs.statSync(dbPath)
-    console.log('文件存在，就往下执行')
-}
-catch (error){
-    console.log('文件不存在，就创建文件')
-    fs.writeFileSync(dbPath,'') //创建数据库文件
-}
-console.log('到这里，文件一定存在')
+ensureDb()
 
-let list,n
+const list = fetch()
+const n = content
 
 switch(verb){
     case 'add':
-        list = fetch()
-        addTask(list,content)
-        save(list)     
+        addTask(list,content) 
         break;
     case 'list':
-        list = fetch()
-        console.log(list)
         break;
     case 'delete':
-        list = fetch()
-        n = content //从1开始
         removeTask(list,n)
-        save(list)
         break;
     case 'done':
-        list = fetch()
-        n = content //从1开始
         taskDone(list,n)
-        save(list)
         break;
-    case 'eidt':
-        list = fetch()
-        n = content //从1开始
+    case 'edit':
         editTask(list,n,content2)
-        save(list)
         break;
     default:
-        console.log('你的动词是：' ,verb)
-        console.log('我不知道你想干嘛')
+        console.log('我不知道你想做什么')
         break;
 }
+display(list)
+if(verb !== 'list'){
+    save(list)
+}
 
-
-
+function ensureDb(){//确保文件一定存在，如果有报错，就执行catch
+    try{
+        fs.statSync(dbPath) //文件存在，什么也不做
+    }
+    catch (error){
+        fs.writeFileSync(dbPath,'') //文件不存在，就创建文件
+    }
+    //到这里，文件一定存在
+}
+function save(list){ //保存到数据库
+    fs.writeFileSync(dbPath,JSON.stringify(list))
+}
+function fetch(){ //list赋值逻辑
+    const fileContent = fs.readFileSync(dbPath).toString()
+    let list
+    try{
+        list = JSON.parse(fileContent) || []
+    }
+    catch(error){
+        list = []
+    }
+    return list
+}
+function addTask(list,content){ //添加todo到list
+    list.push([content,false])
+}
+function removeTask(list,n){//从list移除todo
+    list.splice(n-1,1)
+}
+function taskDone(list,n){ //完成某个todo
+    list[n-1][1] = true
+}
+function editTask(list,n,content2){ //编辑某个todo
+    list[n-1][0] = content2
+}
+function display(list){ //打印list
+    for(let i = 0; i<list.length; i++){
+        const mark = list[i][1] === true?'[O]':'[X]'
+        console.log( mark + ' ' + '任务内容：' + list[i][0])
+    }
+}
 
